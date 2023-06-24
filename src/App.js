41f76f2d348022003c2cc6ee5b47f53275c1e3c4
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-06-24 12:26:13 +0500

    practice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import Navbar from "./components/navbar/Navbar";
+// import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+import About from "./components/about/About"
+import Services from "./components/services/Services"
+import Skills from "./components/skills/Skills"
+import Projects from "./components/projects/Projects"
+import Contact from "./components/contact/Contact"
+
+
 
 function App() {
  
   return (
     <>
-   <Navbar />
+   {/* <Navbar /> */}
+   <Home/>
+   <About/>
+   <Services/>
+   <Skills/>
+   <Projects/>
+   <Contact/>
     </>
   );
 }
---
+++ src/components/skills/Skills.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const skills = () => {
+  return (
+    <>
+      skills
+      
+    </>
+  )
+}
+
+export default skills
