71a9fae9d79dc3296c683b82d04bb2e6d4dd77ee
M ZAIN ALI <107149449+mzain42401@users.noreply.github.com>
2023-06-25 11:17:31 +0500

    practice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-// import Navbar from "./components/navbar/Navbar";
-import Home from "./components/home/Home";
-import About from "./components/about/About"
-import Services from "./components/services/Services"
-import Skills from "./components/skills/Skills"
-import Projects from "./components/projects/Projects"
-import Contact from "./components/contact/Contact"
+import Navbar from "./components/navbar/Navbar";
+// import Home from "./components/home/Home";
+// import About from "./components/about/About"
+// import Services from "./components/services/Services"
+// import Skills from "./components/skills/Skills"
+// import Projects from "./components/projects/Projects"
+// import Contact from "./components/contact/Contact"
 
 
 
@@ -12,13 +12,13 @@ function App() {
  
   return (
     <>
-   {/* <Navbar /> */}
-   <Home/>
+   <Navbar />
+   {/* <Home/>
    <About/>
    <Services/>
    <Skills/>
    <Projects/>
-   <Contact/>
+   <Contact/> */}
     </>
   );
 }
