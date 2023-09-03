27199755300722bd38a0b64432a06f8fef7aa1f7
m zain ali <mzain42401@gmail.com>
2023-09-03 23:57:48 +0500

    aboutpage

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import classes from './about.module.css'
 import MyImage from "../../images/WhatsApp Image 2023-08-29 at 2.32.45 AM.jpeg"
 // import { FaFacebookF } from "react-icons/fa";
 import Typewriter from "typewriter-effect";
 import Btn from '../Btn/Btn';
+import Aos from "aos";
+import 'aos/dist/aos.css'
 
 const About = () => {
   const { aboutPageContainer, aboutCenterDiv,aboutMe, myImageDiv,picBottomBtn, myPersonalImage, aboutMeDiv, aboutTypeWriterdiv, aboutContent, btnDiv,picContentDiv } = classes
+  useEffect(() => {
+    Aos.init({ duration: 1500 })
+  })
   return ( 
     <>
       <div className={aboutPageContainer}>
@@ -15,7 +20,7 @@ const About = () => {
             <h1>Who i am</h1>
           </div>
           <div className={picContentDiv}>
-            <div className={myImageDiv}>
+            <div className={myImageDiv} data-aos="fade-right">
 
               <div className={myPersonalImage}>
 
@@ -28,7 +33,7 @@ const About = () => {
                 <Btn />
               </div>
             </div>
-            <div className={aboutMeDiv}>
+            <div className={aboutMeDiv} data-aos="fade-up">
 
               <div className={aboutTypeWriterdiv}>
                 <span> I'm</span>
