34e292c4b704baefbf6d77032ca3812d17073d53
m zain ali <mzain42401@gmail.com>
2023-09-11 16:39:38 +0500

    backgrounfImg

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,126 +1,95 @@
 import React from 'react'
 import classes from './projects.module.css'
 import Image from '../../images/me.png'
-import Todo from '../../images/todoweb.png'
-import Developer from '../../images/developer.png'
+import Todo from '../../images/mytodos.jpg'
+import Developer from '../../images/Blue Illustration Laptop Hello Circle Sticker.png'
 
+const cardarry=[
+  {
+name:"To-Do APP",
+discription:" Digital task manager, allowing users to add tasks, set due dates, prioritize items, mark tasks as completed.",
+cardImage:Todo,
+github:"https://github.com/mzain42401/todo-web",
+preview:"https://mz-todo.vercel.app/"
+  },
+  {
+    name:"Developer Portfolio",
+    discription:"It's collection of projects, that showcase a developer's skills, expertise, and experience.",
+cardImage:Developer,
+
+    github:"https://github.com/mzain42401/My_Porfolio",
+    preview:"https://mzain.vercel.app/"
+      },{
+        name:"To-Do APP",
+        discription:" Digital task manager, allowing users to add tasks, set due dates, prioritize items, mark tasks as completed.",
+cardImage:Todo,
+
+        github:"https://github.com/mzain42401/todo-web",
+        preview:"https://mz-todo.vercel.app/"
+          },{
+            name:"To-Do APP",
+            discription:" Digital task manager, allowing users to add tasks, set due dates, prioritize items, mark tasks as completed.",
+cardImage:Todo,
+
+            github:"https://github.com/mzain42401/todo-web",
+            preview:"https://mz-todo.vercel.app/"
+              },{
+                name:"To-Do APP",
+                discription:" Digital task manager, allowing users to add tasks, set due dates, prioritize items, mark tasks as completed.",
+cardImage:Todo,
+
+                github:"https://github.com/mzain42401/todo-web",
+                preview:"https://mz-todo.vercel.app/"
+                  },{
+                    name:"To-Do APP",
+                    discription:" Digital task manager, allowing users to add tasks, set due dates, prioritize items, mark tasks as completed.",
+cardImage:Todo,
+
+                    github:"https://github.com/mzain42401/todo-web",
+                    preview:"https://mz-todo.vercel.app/"
+                      }
+]
 
 
 const Projects = () => {
-  const { projectSection, projectCenterDiv, myProject, projectImgDiv, projectImg, projectTittle, projectDivider, projectDiscruption, projectBtnDiv } = classes
+  const { projectSection,container,card,front,back, projectCenterDiv, myProject,  projectTittle, projectDiscription, projectBtnDiv } = classes
   return (
     <>
       <section id='projects' className={projectSection}>
         <h1>My Projects</h1>
+
         <div className={projectCenterDiv}>
-          <div className={myProject}>
-            <div className={projectImgDiv}>
-              <img src={Todo} className={projectImg} alt="" />
-            </div>
-            <div className={projectTittle}>
-             To-Do APP
-            </div>
-            <div className={projectDiscruption}>
-            Digital task manager, allowing users to add tasks, set due dates, prioritize items, mark tasks as completed.
-            </div>
-            <div className={projectDivider}>
+{
+  cardarry.map((elment,index)=>{
+    return <>
 
+<div className={container}>
+            <div className={card}>
+              <div className={front}><img src={elment.cardImage}  /></div>
+              <div className={back}>
+                <div className={myProject}>
+                  <div className={projectTittle}>
+                    {elment.name}
+                  </div>
+                  <div className={projectDiscription}>
+                   {elment.discription}
+                  </div>
+
+                  <div className={projectBtnDiv}>
+                    <div > <button><a rel="noreferrer" target='_blank' href={elment.github}>Github</a></button> </div>
+                    <div> <button><a rel="noreferrer" target='_blank' href={elment.preview}>Preview</a></button> </div>
+
+
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className={projectBtnDiv}>
-              <div > <button><a rel="noreferrer" href="https://github.com/mzain42401/todo-web" target='_blank'>Github</a></button> </div>
-              <div> <button><a rel="noreferrer" href="https://mz-todo.vercel.app/" target='_blank'>Preview</a></button> </div>
-
-
-            </div>
-
+          </div>
+    </>
+  })
+}
           </div>
 
-          <div className={myProject}>
-            <div className={projectImgDiv}>
-              <img src={Developer} className={projectImg} alt="" />
-            </div>
-            <div className={projectTittle}>
-            Developer Portfolio
-            </div>
-            <div className={projectDiscruption}>
-            It's collection of  projects, that showcase a developer's skills, expertise, and experience.
-            </div>
-            <div className={projectDivider}>
-
-            </div>
-            <div className={projectBtnDiv}>
-              <div > <button><a rel="noreferrer" href="https://github.com/mzain42401/My_Porfolio" target='_blank'>Github</a></button> </div>
-              <div> <button><a rel="noreferrer" href="https://mzain.vercel.app/" target='_blank'>Preview</a></button> </div>
-
-
-            </div>
-
-          </div>
-          <div className={myProject}>
-            <div className={projectImgDiv}>
-              <img src={Image} className={projectImg} alt="" />
-            </div>
-            <div className={projectTittle}>
-              recaet tofo
-            </div>
-            <div className={projectDiscruption}>
-              akjd jgajjdja jhagjhdgja hjgahdn hvhjdy fdf fgdga vathfdf
-            </div>
-            <div className={projectDivider}>
-
-            </div>
-            <div className={projectBtnDiv}>
-              <div > <button><a rel="noreferrer" href="">Github</a></button> </div>
-              <div> <button><a rel="noreferrer" href="">Preview</a></button> </div>
-
-
-            </div>
-
-          </div>
-          <div className={myProject}>
-            <div className={projectImgDiv}>
-              <img src={Image} className={projectImg} alt="" />
-            </div>
-            <div className={projectTittle}>
-              recaet tofo
-            </div>
-            <div className={projectDiscruption}>
-              akjd jgajjdja jhagjhdgja hjgahdn hvhjdy fdf fgdga vathfdf
-            </div>
-            <div className={projectDivider}>
-
-            </div>
-            <div className={projectBtnDiv}>
-              <div > <button><a rel="noreferrer" href="">Github</a></button> </div>
-              <div> <button><a rel="noreferrer" href="">Preview</a></button> </div>
-
-
-            </div>
-
-          </div>
-          <div className={myProject}>
-            <div className={projectImgDiv}>
-              <img src={Image} className={projectImg} alt="" />
-            </div>
-            <div className={projectTittle}>
-              recaet tofo
-            </div>
-            <div className={projectDiscruption}>
-              akjd jgajjdja jhagjhdgja hjgahdn hvhjdy fdf fgdga vathfdf
-            </div>
-            <div className={projectDivider}>
-
-            </div>
-            <div className={projectBtnDiv}>
-              <div > <button><a rel="noreferrer" href="">Github</a></button> </div>
-              <div> <button><a rel='noreferrer' href="">Preview</a></button> </div>
-
-
-            </div>
-
-          </div>
-          
-        </div>
 
 
       </section>
@@ -128,4 +97,8 @@ const Projects = () => {
   )
 }
 
+
 export default Projects
+
+
+
